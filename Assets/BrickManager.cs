1f07f2fe8985e68f67d2291313b5401060729cc8
Id Raja <iraja@nerdery.com>
2017-06-08 16:34:05 -0500

    gaze working

---
+++ Assets/BrickManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickManager : MonoBehaviour {
+    Renderer rend;
+    private Color originalColor;
+
+    void Awake()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        originalColor = rend.material.color;
+    }
+
+    public void GazeEntered()
+    {
+        rend.material.color = Color.red;
+    }
+
+    public void GazeExited()
+    {
+        rend.material.color = originalColor;
+    }
+}
