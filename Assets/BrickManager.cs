29352ab60f2cf47f16afcd9f12a2defae8af78d2
Id Raja <iraja@nerdery.com>
2017-06-21 14:48:52 -0500

    move camera closer

--- Assets/BrickManager.cs
+++ Assets/BrickManager.cs
@@ -5,27 +5,26 @@ using HoloToolkit.Unity.InputModule;
 
 
 public class BrickManager : MonoBehaviour, IManipulationHandler {
+    private Rigidbody rb;
+    private Collider collider;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
+        collider = GetComponent<Collider>();
 	}
 
     public void OnManipulationStarted(ManipulationEventData eventData)
     {
-
     }
 
     public void OnManipulationUpdated(ManipulationEventData eventData) {
-
     }
 
     public void OnManipulationCompleted(ManipulationEventData eventData) {
-
     }
 
     public void OnManipulationCanceled(ManipulationEventData eventData) {
-
     }
 	
 	// Update is called once per frame
